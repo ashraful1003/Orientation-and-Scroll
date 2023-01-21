d9bc6d503f4a0a69790d016c78809a7d1ea16304
Ashraful Alam <alamashraful1003@gmail.com>
2023-01-21 13:06:03 +0600

    all layout added

--- app/src/main/java/com/softocular/orientationandscroll/MainActivity.java
+++ app/src/main/java/com/softocular/orientationandscroll/MainActivity.java
@@ -2,6 +2,7 @@ package com.softocular.orientationandscroll;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /// if you want to relative layout just remove the comment from the below line
+//        startActivity(new Intent(MainActivity.this, RelativeLayoutActivity.class));
+
+/// if you want to table layout just remove the comment from the below line
+//        startActivity(new Intent(MainActivity.this, TableLayoutActivity.class));
+
+        /// if you want to frame layout just remove the comment from the below line
+//        startActivity(new Intent(MainActivity.this, FrameLayoutActivity.class));
     }
 }
